f4733dcde38984796463173b387b8f8af26dfdcc
Atsu0703 <chaudhariatharva01@gmail.com>
2026-06-28 14:48:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
